759d3d281e4b02f579cf9c91fc7aea2c9ed8a2a0
LDZ-27 <43908812+LDZ-27@users.noreply.github.com>
2023-02-05 02:13:28 +0800

    Add files via upload

---
+++ github.find_more.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s -m 10 --retry 1024 --http2 -H 'Host: github.com' "https://github.com/$1" -o github_profile_$1
+cat github_profile_$1 | grep "href=" | grep -E "@|phone|mail" | grep '#' | sed -e 's/</ /g' -e 's/>/ /g' -e 's/"/ /g' -e 's/{/ /g' -e 's/}/ /g' -e 's/\[/ /g' -e 's/\]/ /g' |  awk '{for(i=1;i<=NF;i++){print $i}}' | grep '@'
+rm -f github_profile_$1
